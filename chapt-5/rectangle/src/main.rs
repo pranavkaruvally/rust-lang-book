d5ddcd0484838d05895c43c7e52598d409e48609
Pranav P <pranavsdreams@gmail.com>
2023-11-11 00:00:40 +0530

    Added area method to Rectangle struct

--- chapt-5/rectangle/src/main.rs
+++ chapt-5/rectangle/src/main.rs
@@ -3,15 +3,24 @@ struct Rectangle {
     breadth: u32
 }
 
+//We add the same area functionality using method
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.breadth
+    }
+}
+
 fn main() {
     let r = Rectangle {
         length: 12,
         breadth: 10
     };
-    let a = area(r);
+    let a = area(&r);
     println!("Area is {}", a);
+    println!("Area using method is {}", r.area());
 }
 
-fn area(r: Rectangle) -> u32 {
+fn area(r: &Rectangle) -> u32 {
     r.length * r.breadth
 }
